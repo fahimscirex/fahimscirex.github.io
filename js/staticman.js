e1c9492ff8257815d103424309f75404abd7a8c6
fahimscirex <fahimscirex@tutanota.com>
2021-02-22 12:28:14 +0000

    deploy: f48b3d1cb8acca21de18d3087a0adf925a78eb76

--- js/staticman.js
+++ js/staticman.js
@@ -5,7 +5,7 @@ $( document ).ready(function() {
 
   $('.js-form').submit(function () {
     var form = this;
-    let url = '{{ .api }}';
+    let url = '{{ .Site.Params.staticman.api }}';
     let data = $(this).serialize();
 
     $(form).addClass('form--loading');
